1bc57c1882c3a18a85f4ac3efdfe906df18225e1
lihuang1026 <lihuang1026@gmail.com>
2024-08-05 00:33:15 -0400

    finish spring core

---
+++ 02-spring-boot-spring-core/08-bean-lifecyle-methods/src/main/java/com/huangli/springcoredemo/common/trackCoach.java
@@ -0,0 +1,20 @@
+package com.huangli.springcoredemo.common;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Primary
+//@Lazy
+public class trackCoach implements Coach {
+
+    public trackCoach(){
+        System.out.println("In constructor"+getClass().getSimpleName());
+    }
+    @Override
+    public String getDailyWorkout() {
+        System.out.println("In constructor: "+getClass());
+        return "Run a hard 5k!";
+    }
+}
