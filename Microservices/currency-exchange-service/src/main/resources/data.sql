34982e3721588b7a388ab75835874dcaab25f96e
Huang Li <lihuang1026@gmail.com>
2024-09-11 17:47:22 -0400

    microservice practice

---
+++ Microservices/currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,23 @@
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(1001, 'USD', 'INR', 65, '');
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(1002, 'EUR', 'INR', 75, '');
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(1003, 'AUD', 'INR', 25, '');
+
+--insert into currency_exchange
+--(id, currency_from, currency_to, conversion_multiple)
+--values(1001, 'USD', 'INR', 65);
+--insert into currency_exchange
+--(id, currency_from, currency_to, conversion_multiple)
+--values(1002, 'EUR', 'INR', 75);
+--insert into currency_exchange
+--(id, currency_from, currency_to, conversion_multiple)
+--values(1003, 'AUD', 'INR', 25);
+--CONVERSION_MULTIPLE  	ID  	CURRENCY_FROM  	CURRENCY_TO  	ENVIRONMENT
+--65.00	1001	USD	INR	null
+--75.00	1002	EUR	INR	null
+--25.00	1003	AUD	INR	null
